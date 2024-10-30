0e34613654c0359b017e19b2c9a9aa0fa24194a7
Tanguy Bron <63880113+tanguybron@users.noreply.github.com>
2024-10-30 16:31:26 +0100

    customize KDE

--- design_install.sh
+++ design_install.sh
@@ -1,6 +1,9 @@
 ## changing to dark theme 'Adwaita-dark'
-gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
+
+wget "https://images.pling.com/img/00/00/49/90/47/2217376/logo-2.jpg" -O $HOME/background.jpg
+
+plasma-apply-lookandfeel -a org.kde.breezedark.desktop
+plasma-apply-wallpaperimage $HOME/background.jpg
 
 ## install OhMyZsh
-apt install zsh
 bash -x ./user_terminal.sh
