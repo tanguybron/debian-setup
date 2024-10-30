a565c0707598d3ccf9c5890101c1fbcc969180d8
Tanguy Bron <63880113+tanguybron@users.noreply.github.com>
2024-10-30 17:20:03 +0100

    correct bash script

--- main_install.sh
+++ main_install.sh
@@ -1,6 +1,6 @@
 echo "Do you want to execute pre-installation script ? [Y/n]"
 read answer
-if [answer != 'n'] 
+if ["$answer" != "n"] 
 then 
     bash pre_install.sh
 fi 
@@ -8,7 +8,7 @@ fi
 
 echo "Do you want to execute design changer script ? [Y/n]"
 read answer
-if [answer != 'n'] 
+if ["$answer" != "n"] 
 then 
     bash design_install.sh
 fi 
@@ -17,7 +17,7 @@ fi
 
 echo "Do you want to install all pentesting tools ? [Y/n]"
 read answer
-if [answer != 'n'] 
+if ["$answer" != "n"] 
 then 
     bash tools_install.sh
 fi 
